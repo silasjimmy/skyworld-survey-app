b488ee317a77cd400b19bc36359163b546a80fa5
Silas Jimmy <jimmysilas17@gmail.com>
2025-04-07 06:34:34 +0300

    refactor: implement api endpoints

--- simple-survey-api/flaskr/__init__.py
+++ simple-survey-api/flaskr/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
-from database.models import setup_database
+from flask import Flask, jsonify, abort, request
+from database.models import setup_database, Question, Response, Certificate
 from flask_cors import CORS
+from flask_swagger import swagger
 
 
 def create_app(test_config=None):
@@ -18,12 +19,126 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Headers',
                              'Content-Type, Authorization, true')
         response.headers.add('Access-Control-Allow-Methods',
-                             'GET, PATCH, POST, DELETE, OPTIONS')
+                             'GET, PUT')
         return response
 
     @app.route('/')
-    def home():
-        return 'Sky World Survey App!'
+    def docs():
+        """
+        API Documentation with Swagger
+        ---
+        tags:
+          - docs
+        responses:
+          200:
+            description: documentation retrieved successfully
+        """
+        swag = swagger(app)
+        swag['info']['version'] = "1.0"
+        swag['info']['title'] = "Sky Survey API"
+        return jsonify({
+            "status": 200,
+            "success": True,
+            "docs": swag
+        })
+
+    @app.route('/api/questions', methods=['GET'])
+    def get_questions():
+        """
+        Get a list of all survey questions
+        ---
+        tags:
+          - questions
+        responses:
+          200:
+            description: questions retrieved successfully
+        """
+        try:
+            questions_query = Question.query.all()
+
+            questions = [question.format() for question in questions_query]
+
+            return jsonify({
+                "status": 200,
+                "success": True,
+                "questions": questions
+            })
+        except Exception as e:
+            abort(422)
+
+    @app.route('/api/questions/responses', methods=['GET'])
+    def get_responses():
+        """
+        Get a list of all survey responses
+        ---
+        tags:
+          - responses
+        responses:
+          200:
+            description: responses retrieved successfully
+        """
+        try:
+            responses_query = Response.query.all()
+
+            responses = [response.format() for response in responses_query]
+
+            return jsonify({
+                "status": 200,
+                "success": True,
+                "responses": responses
+            })
+        except Exception as e:
+            abort(422)
+
+    @app.route('/api/questions/responses/certificates/<int:certificate_id>', methods=['GET'])
+    def get_certificate(certificate_id):
+        """
+        Get a survey response certificate by id
+        ---
+        tags:
+          - certificate
+        responses:
+          200:
+            description: certificate retrieved successfully
+        """
+        try:
+            certificate = Certificate.query.filter(
+                Certificate.id == certificate_id).one_or_none()
+
+            return jsonify({
+                "status": 200,
+                "success": True,
+                "certificate": certificate
+            })
+        except Exception as e:
+            abort(422)
+
+    @app.route('/api/questions/responses', methods=['PUT'])
+    def add_response():
+        """
+        Save a user survey response
+        ---
+        tags:
+          - response
+        responses:
+          200:
+            description: responses uploaded successfully
+        """
+        try:
+            response_data = request.form
+
+            # response_files = request.files
+            # print(response_files)
+
+            new_response = Response(**response_data)
+
+            return jsonify({
+                "status": 200,
+                "success": True,
+                "response": new_response.format()
+            })
+        except Exception as e:
+            abort(422)
 
     # Error Handling
     @app.errorhandler(422)
